903f62b3ac49544c20a299534804ec6a590b62df
UNAINR <munain605@gmail.com>
2024-11-28 16:20:59 +0500

    sss

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk'])
+const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/'])
 
 export default clerkMiddleware();
 
