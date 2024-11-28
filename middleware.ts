fa8050224d2623b4da6a65b1a3220b74a64aa1ba
UNAINR <munain605@gmail.com>
2024-11-28 20:11:33 +0500

    change

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Public routes that don't require authentication
-    "/",
-    "/api/webhooks/clerk",
-    // Protect all other routes, including API routes
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
