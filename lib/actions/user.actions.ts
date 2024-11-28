6636fdebbecfdaf5a45ba4290a47837a1fda5057
UNAINR <munain605@gmail.com>
2024-11-28 09:11:52 +0500

    mainchanges

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,4 @@
 "use server";
-
 import { revalidatePath } from "next/cache";
 import User from "../Database/models/user.model";
 import connectDB from "../Database/mongoose";
--- types/index.d.ts
+++ types/index.d.ts
@@ -5,14 +5,14 @@ declare type CreateUserParams = {
     clerkId: string;
     email: string;
     username: string;
-    firstName: string;
-    lastName: string;
+    firstName: string?;
+    lastName: string?;
     photo: string;
   };
   
   declare type UpdateUserParams = {
-    firstName: string;
-    lastName: string;
+    firstName: string?;
+    lastName: string?;
     username: string;
     photo: string;
   };
